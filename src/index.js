13e755ad79a221653014be06ec232f61f31ab787
Joe Dei Rossi <deirossijoseph@gmail.com>
2019-03-26 17:40:42 -0700

    Completed 'React Intro' prototype

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+var user = {
+    name: 'STEVE',
+    luckyNumber: luckyNumber()
+};
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    greeting(user),
     document.getElementById('root')
 );
+
+function luckyNumber() {
+    var randomNumber = Math.floor(Math.random() * 1000) + 1;
+    return randomNumber;
+}
+
+function greeting(userObject) {
+    return (
+        <div className="container">
+            <h1>WHAT'S UP {userObject.name}!</h1>
+            <h2 className="text-muted">YOUR LUCKY NUMBER IS {userObject.luckyNumber}!</h2>
+        </div>
+    )
+}
