f62df043d8ba796777198141269ee79b91f88034
Joe Dei Rossi <deirossijoseph@gmail.com>
2019-03-31 15:05:32 -0700

    Completed 'React Handling Inputs/Forms' prototype

---
+++ src/data/contacts.js
@@ -0,0 +1,21 @@
+
+export default [
+    {
+        firstName: 'Santa',
+        lastName: 'Claus',
+        phone: '247-478-6270',
+        email: 'notMadeUp@google.com'
+    },
+    {
+        firstName: 'Easter',
+        lastName: 'Bunny',
+        phone: '344-486-8464',
+        email: 'totallyReal@google.com'
+    },
+    {
+        firstName: 'Tooth',
+        lastName: 'Fairy',
+        phone: '227-448-3384',
+        email: 'definitelyExists@google.com'
+    }
+];
